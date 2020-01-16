68b3563f616a0d1c2457fe2b965f96f163c1b248
Paul Kim <pkim1295@gmail.com>
2020-01-16 03:53:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,7 +54,7 @@ def total_gross(source)
   count = 0
   
   while count < dir_total.length do
-    if names[count] == dir_total
+    if names[count] == dir_total[]
       total += dir_total[:name]
       count += 1
     end
